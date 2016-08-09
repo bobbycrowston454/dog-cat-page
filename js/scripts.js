956bae18b2984017de647f20f8493d01f0edd648
Bobby Crowston and Kiet Duong <kiet@>
2016-08-09 15:44:41 -0700

    this added 2 more buttons and a picture that will change with background color changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#dog").prepend("<li>WOOF!</li>");
     $("ul#cat").prepend("<li>MEOW!</li>");
+    $("ul#cat").before("<p>WOW! Beautiful...</p>")
   });
 
   $("button#goodbye").click(function() {
     $("ul#dog").prepend("<li>WOOOF!</li>");
     $("ul#cat").prepend("<li>MEOW!</li>");
+    $("ul#dog").before("<p>WOW! SO COOL!</p>")
   });
 
   $("button#stop").click(function() {
@@ -16,5 +18,9 @@ $(document).ready(function() {
   $("button#white").click(function() {
     $("body").removeClass();
     $("body").addClass("black-background");
+  });
+  $("button#picture").click(function() {
+    $("#doggy").toggle();
+    $("#cat-rm").toggle();
   });
 });
